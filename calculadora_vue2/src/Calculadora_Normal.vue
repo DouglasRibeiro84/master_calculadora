43f2a14eb7217baf5c31be93f49f25d3d3e60d57
doouglas <douglasribeiro084@gmail.com>
2024-11-13 15:02:48 -0300

    Finalizado a parte visual

--- calculadora_vue2/src/Calculadora_Normal.vue
+++ calculadora_vue2/src/Calculadora_Normal.vue
@@ -1,23 +1,30 @@
 <script>
+import { reactive } from 'vue';
+
 export default {
     data() {
         return {
             evento: false,
+            numeroAtual: 0,
         };
     },
     methods: {
-        alterarEstilo(){
-            
+        alterarEstilo() {
+        },
+        adicionarNumero(numero) {
+            console.log('Botão clicado, número:', numero);
+            this.numeroAtual =+ numero ;
         }
-    }
-};
+    },
+}
+
 
 </script>
 
 <template>
     <div class="container" :class="{ dark: evento }">
         <div class="form-check form-switch">
-            <h1 class="p-5 text-center">Escola o tipo de calculadora <select>
+            <h1 class="p-5 text-center">Escola o tipo de calculadora:<select class="p-3 rounded ms-4">
                     <option value="">normal</option>
                     <option value="">Regra de 3</option>
                 </select>
@@ -31,7 +38,7 @@ export default {
                     <div class="card">
                         <div class="card-body p-4">
                             <div class="row p-2">
-                                <input class="w-100 p-2 text-end" type="number" name="" id="">
+                                <input v-model="numeroAtual" class="w-100 p-2 text-end visor" type="number" readonly />
                             </div>
                             <div class="row mt-3 d-flex justify-content-center pb-2">
                                 <div class="col-md-3 d-flex justify-content-center">
@@ -49,13 +56,13 @@ export default {
                             </div>
                             <div class="row mt-3 d-flex justify-content-center">
                                 <div class="col-md-3 d-flex justify-content-center">
-                                    <button class="btn btn-primary ps-4 pe-4">7</button>
+                                    <button @click="adicionarNumero(7)" class="btn btn-numbers ps-4 pe-4">7</button>
                                 </div>
                                 <div class="col-md-3 d-flex justify-content-center">
-                                    <button class="btn btn-primary ps-4 pe-4">8</button>
+                                    <button @click="adicionarNumero(8)"  class="btn btn-numbers ps-4 pe-4">8</button>
                                 </div>
                                 <div class="col-md-3 d-flex justify-content-center">
-                                    <button class="btn btn-primary ps-4 pe-4">9</button>
+                                    <button class="btn btn-numbers ps-4 pe-4">9</button>
                                 </div>
                                 <div class="col-md-3 d-flex justify-content-center">
                                     <button class="btn btn-dark ps-4 pe-4">x</button>
@@ -63,13 +70,13 @@ export default {
                             </div>
                             <div class="row mt-3 d-flex justify-content-center">
                                 <div class="col-md-3 d-flex justify-content-center">
-                                    <button class="btn btn-primary ps-4 pe-4">4</button>
+                                    <button class="btn btn-numbers ps-4 pe-4">4</button>
                                 </div>
                                 <div class="col-md-3 d-flex justify-content-center">
-                                    <button class="btn btn-primary ps-4 pe-4">5</button>
+                                    <button class="btn btn-numbers ps-4 pe-4">5</button>
                                 </div>
                                 <div class="col-md-3 d-flex justify-content-center">
-                                    <button class="btn btn-primary ps-4 pe-4">6</button>
+                                    <button class="btn btn-numbers ps-4 pe-4">6</button>
                                 </div>
                                 <div class="col-md-3 d-flex justify-content-center">
                                     <button class="btn btn-dark ps-4 pe-4">-</button>
@@ -77,13 +84,13 @@ export default {
                             </div>
                             <div class="row mt-3 d-flex justify-content-center">
                                 <div class="col-md-3 d-flex justify-content-center">
-                                    <button class="btn btn-primary ps-4 pe-4">1</button>
+                                    <button class="btn btn-numbers ps-4 pe-4"> 1 </button>
                                 </div>
                                 <div class="col-md-3 d-flex justify-content-center">
-                                    <button class="btn btn-primary ps-4 pe-4">2</button>
+                                    <button class="btn btn-numbers ps-4 pe-4">2</button>
                                 </div>
                                 <div class="col-md-3 d-flex justify-content-center">
-                                    <button class="btn btn-primary ps-4 pe-4">3</button>
+                                    <button class="btn btn-numbers ps-4 pe-4">3</button>
                                 </div>
                                 <div class="col-md-3 d-flex justify-content-center">
                                     <button class="btn btn-dark ps-4 pe-4">+</button>
@@ -94,10 +101,10 @@ export default {
                                     <button class="btn btn-dark ps-3 pe-3">+/-</button>
                                 </div>
                                 <div class="col-md-3 d-flex justify-content-center">
-                                    <button class="btn btn-primary ps-4 pe-4">0</button>
+                                    <button class="btn btn-numbers ps-4 pe-4">0</button>
                                 </div>
                                 <div class="col-md-3 d-flex justify-content-center">
-                                    <button class="btn btn-primary ps-4 pe-4">,</button>
+                                    <button class="btn btn-dark ps-4 pe-4">,</button>
                                 </div>
                                 <div class="col-md-3 d-flex justify-content-center">
                                     <button class="btn btn-success ps-4 pe-4">=</button>
@@ -113,12 +120,146 @@ export default {
 
 <style scoped>
 .container {
-    background-color: rgb(184, 61, 153);
+    background-image: url(./images/background_img.png);
+    background-size: cover;
+    background-repeat: no-repeat;
     height: 100vh;
+    max-width: 100%;
+    transition: .8s ease-in-out;
 }
 
 .container.dark {
-    background-color: rgba(0, 0, 0, 0.582);
-    height: 100vh;
+    background-image: url(./images/background_dark.png);
+    transition: .8s ease-in-out;
+}
+
+.container.dark h1 {
+    color: white;
+    text-shadow: 4px 4px 0px black;
+}
+
+select {
+    text-align: center;
+    background-color: rgba(8, 8, 8, 0.712);
+    color: white;
+    outline: none;
+    border: none;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    box-shadow: 0 0 0 .10rem rgba(255, 255, 255, 0.397);
+}
+
+h1 {
+    font-family: "Rubik Glitch", system-ui;
+    font-weight: 400;
+    font-style: normal;
+    color: #20083A;
+    text-shadow: 4px 4px 0px white;
+    font-size: 40px;
+    line-height: 50px;
+}
+
+.visor {
+    outline: none;
+    border: none;
+    background-color: #043513cc;
+    border-radius: 10px;
+    border: 1px solid #24f04971;
+    font-family: "Rubik Gemstones", system-ui;
+    color: #4DE35E;
+    font-size: 30px;
+}
+
+.visor::-webkit-outer-spin-button,
+.visor::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+}
+
+.form-check-input {
+    --bs-form-check-bg: #f9f9f9c4
+}
+
+.form-check-input:focus {
+    border-color: transparent;
+    box-shadow: 0 0 0 .10rem rgba(255, 255, 255, 0.233)
+}
+
+.form-check-input:checked {
+    border: transparent;
+    background-color: rgba(255, 255, 255, 0.11);
+    box-shadow: 0 0 0 .10rem rgba(255, 255, 255, 0.233)
+}
+
+@keyframes changeBoxShadowColorDark {
+    0% {
+        box-shadow: 0 0px 10px .15rem rgba(248, 248, 248, 0.5);
+    }
+
+    33% {
+        box-shadow: 0 0px 10px .15rem rgb(81, 72, 87);
+    }
+
+    66% {
+        box-shadow: 0 0px 10px .15rem rgba(53, 53, 88, 0.993);
+    }
+
+    100% {
+        box-shadow: 0 0px 10px .15rem rgba(255, 255, 255, 0.63);
+    }
+}
+
+.container.dark .card {
+    animation: changeBoxShadowColorDark 4s cubic-bezier(0.25, 0.8, 0.25, 1) infinite;
+}
+
+@keyframes changeBoxShadowColor {
+    0% {
+        box-shadow: 0 0px 10px .15rem rgba(248, 248, 248, 0.5);
+    }
+
+    33% {
+        box-shadow: 0 0px 10px .15rem rgb(40, 54, 241);
+    }
+
+    66% {
+        box-shadow: 0 0px 10px .15rem rgb(1, 223, 252);
+    }
+
+    100% {
+        box-shadow: 0 0px 10px .15rem rgba(255, 255, 255, 0.63);
+    }
+}
+
+.card {
+    animation: changeBoxShadowColor 4s cubic-bezier(0.25, 0.8, 0.25, 1) infinite;
+}
+
+.card {
+    background-color: #20083ab9;
+}
+
+.container.dark .card {
+    background-color: #131214b7;
+}
+
+.btn,
+.btn:hover {
+    border: 1px solid rgba(255, 255, 255, 0.664);
+    font-family: "Rowdies", sans-serif;
+    font-size: 18px;
+}
+
+.btn.btn-numbers {
+    background-color: #CE5DD9;
+    color: white;
+    font-family: "Rowdies", sans-serif;
+    font-size: 24px;
+    width: 100%;
+}
+
+.container.dark .btn.btn-numbers {
+    background-color: #0B0B0B
 }
 </style>
